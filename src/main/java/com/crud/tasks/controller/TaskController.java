fb454da538e1196e8b6c21b15835183a48e287ea
ZadrogaB <zadroga.b@wp.pl>
2021-06-23 14:40:34 +0200

    32.4. Ostatnia refaktoryzacja REST API

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
-@RequestMapping("/v1/task")
+@RequestMapping("/v1")
 public class TaskController {
 
     private final DbService service;
@@ -24,34 +24,34 @@ public class TaskController {
         this.taskMapper = taskMapper;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    @RequestMapping(method = RequestMethod.GET, value = "/tasks")
     public List<TaskDto> getTasks() {
         List<Task> tasks = service.getAllTasks();
         return taskMapper.mapToTaskDtoList(tasks);
     }
 
-    @GetMapping(value = "getTask")
+    @GetMapping(value = "/tasks/{taskId}")
     public TaskDto getTask(@RequestParam Long taskId) throws TaskNotFoundException {
         return taskMapper.mapToTaskDto(
                 service.getTask(taskId).orElseThrow(TaskNotFoundException::new)
         );
     }
 
-    @DeleteMapping(value = "deleteTask")
+    @DeleteMapping(value = "/tasks/{taskId}")
     public TaskDto deleteTask(@RequestParam Long taskId) throws TaskNotFoundException {
         TaskDto taskDto = getTask(taskId);
         service.deleteTask(taskId);
         return taskDto;
     }
 
-    @PutMapping(value = "updateTask")
+    @PutMapping(value = "/tasks")
     public TaskDto updateTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         Task savedTask = service.saveTask(task);
         return taskMapper.mapToTaskDto(savedTask);
     }
 
-    @PostMapping(value = "createTask",  consumes = MediaType.APPLICATION_JSON_VALUE)
+    @PostMapping(value = "/tasks",  consumes = MediaType.APPLICATION_JSON_VALUE)
     public void createTask(@RequestBody TaskDto taskDto) {
         Task task = taskMapper.mapToTask(taskDto);
         service.saveTask(task);
